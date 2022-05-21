48b7a744aed3e27c8f45419f231c0e5837e79ba0
Michael Ivashko <official.michael.developer@gmail.com>
2022-05-21 23:53:09 +0300

    Preparing for Hero section development

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,18 @@ import * as React from 'react'
 // styles
 import '../reset.css'
 // components
+import {Hero} from '../sections/Hero'
 import {AboutFund} from '../sections/AboutFund'
 import {OurMission} from '../sections/OurMission'
 import {KindOfActivity} from '../sections/KindOfActivity'
+import {OurPartners} from '../sections/OurPartners'
 
 export default function Home() {
   return <main>
+    <Hero/>
     <AboutFund/>
     <OurMission/>
     <KindOfActivity/>
+    <OurPartners/>
   </main>
 }
