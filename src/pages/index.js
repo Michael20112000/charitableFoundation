f421195f36abfed14f57f56673fa224e017d71af
Michael Ivashko <official.michael.developer@gmail.com>
2022-05-21 23:48:50 +0300

    KindOfActivity section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,12 @@ import '../reset.css'
 // components
 import {AboutFund} from '../sections/AboutFund'
 import {OurMission} from '../sections/OurMission'
+import {KindOfActivity} from '../sections/KindOfActivity'
 
 export default function Home() {
   return <main>
     <AboutFund/>
     <OurMission/>
+    <KindOfActivity/>
   </main>
 }
